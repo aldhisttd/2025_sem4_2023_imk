a9643c9098ba239681f260e067f351212ead7cf9
Aldhi <aldhi.albadri@gmail.com>
2025-03-10 13:00:03 +0700

    add responsive module

---
+++ pagib/bootstrap/responsive1.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  </head>
+  <body>
+    
+    <div class="row">
+        <div class="col-12 col-sm-6 col-md-3 bg-danger">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ullam architecto sit magni incidunt sequi, ipsam, neque autem voluptas sapiente necessitatibus debitis voluptatibus consequatur accusantium minus expedita modi alias soluta libero.
+        </div>
+        <div class="col-12 col-sm-6 col-md-3 bg-primary">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ullam architecto sit magni incidunt sequi, ipsam, neque autem voluptas sapiente necessitatibus debitis voluptatibus consequatur accusantium minus expedita modi alias soluta libero.
+        </div>
+        <div class="col-12 col-sm-6 col-md-3 bg-warning">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ullam architecto sit magni incidunt sequi, ipsam, neque autem voluptas sapiente necessitatibus debitis voluptatibus consequatur accusantium minus expedita modi alias soluta libero.
+        </div>
+        <div class="col-12 col-sm-6 col-md-3 bg-secondary">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ullam architecto sit magni incidunt sequi, ipsam, neque autem voluptas sapiente necessitatibus debitis voluptatibus consequatur accusantium minus expedita modi alias soluta libero.
+        </div>
+    </div>
+
+    <!-- <hr>
+
+    <div class="row">
+        <div class="col-sm-6 col-12 bg-danger">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos libero, sint sunt, ipsam voluptas, delectus doloribus impedit iure deserunt animi numquam nostrum porro iusto error cum blanditiis quae eos a?
+        </div>
+        <div class="col bg-warning">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos libero, sint sunt, ipsam voluptas, delectus doloribus impedit iure deserunt animi numquam nostrum porro iusto error cum blanditiis quae eos a?
+        </div>
+    </div> -->
+
+
+
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  </body>
+</html>
