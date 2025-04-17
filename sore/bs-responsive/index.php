687f2d03e7961bc2a0f7692391fbce76aa0c9e26
Aldhi <aldhi.albadri@gmail.com>
2025-04-17 21:08:37 +0700

    fix

---
+++ sore/bs-responsive/index.php
@@ -0,0 +1,87 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
+</head>
+
+<body>
+
+    <div class="container-fluid">
+
+        <div class="row">
+            <div class="col-12 col-md-6 bg-danger">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti in a molestias doloribus nesciunt, eos nemo? Neque adipisci minus, nesciunt temporibus quam, perferendis corrupti minima rem, est iusto ducimus eum.
+            </div>
+            <div class="col-12 col-md-6 bg-warning">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime obcaecati aliquam itaque natus facilis vel alias totam saepe, eligendi eveniet deserunt, eius nemo quo possimus consequuntur quidem quia reprehenderit quam!
+            </div>
+        </div>
+
+    </div>
+
+    <br>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12 col-sm-6 col-md bg-danger">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime obcaecati aliquam itaque natus facilis vel alias totam saepe, eligendi eveniet deserunt, eius nemo quo possimus consequuntur quidem quia reprehenderit quam!
+            </div>
+            <div class="col-12 col-sm-6 col-md bg-warning">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime obcaecati aliquam itaque natus facilis vel alias totam saepe, eligendi eveniet deserunt, eius nemo quo possimus consequuntur quidem quia reprehenderit quam!
+            </div>
+            <div class="col-12 col-sm-12 col-md bg-success">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime obcaecati aliquam itaque natus facilis vel alias totam saepe, eligendi eveniet deserunt, eius nemo quo possimus consequuntur quidem quia reprehenderit quam!
+            </div>
+        </div>
+    </div>
+
+    <br>
+
+    <div class="container-fluid">
+
+        <div class="row">
+
+            <div class="col-12 col-md-6 bg-danger">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti in a molestias doloribus nesciunt, eos nemo? Neque adipisci minus, nesciunt temporibus quam, perferendis corrupti minima rem, est iusto ducimus eum.
+            </div>
+            <div class="col-12 col-md-6 d-none d-md-block bg-warning">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime obcaecati aliquam itaque natus facilis vel alias totam saepe, eligendi eveniet deserunt, eius nemo quo possimus consequuntur quidem quia reprehenderit quam!
+            </div>
+            <div class="col-12 col-md-6 d-block d-md-none bg-primary">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime obcaecati aliquam itaque natus facilis vel alias totam saepe, eligendi eveniet deserunt, eius nemo quo possimus consequuntur quidem quia reprehenderit quam!
+            </div>
+
+        </div>
+
+    </div>
+
+    <br>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-6 bg-danger d-sm-none d-md-block">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi alias, nihil atque eveniet nobis sint architecto magnam! Vero, repellat! Ab vero similique quas sapiente a eaque nobis ipsum, consectetur dolor!
+            </div>
+            <div class="col bg-success">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi alias, nihil atque eveniet nobis sint architecto magnam! Vero, repellat! Ab vero similique quas sapiente a eaque nobis ipsum, consectetur dolor!
+            </div>
+            <div class="col bg-primary">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi alias, nihil atque eveniet nobis sint architecto magnam! Vero, repellat! Ab vero similique quas sapiente a eaque nobis ipsum, consectetur dolor!
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col bg-info">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam quo iste asperiores sequi exercitationem hic quaerat expedita perspiciatis nobis necessitatibus. Ducimus, dolorem fugiat ad nulla quis dolore non necessitatibus eum?
+            </div>
+        </div>
+    </div>
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js" integrity="sha384-k6d4wzSIapyDyv1kpU366/PK5hCdSbCRGRCMv+eplOQJWyd1fbcAu9OCUj5zNLiq" crossorigin="anonymous"></script>
+</body>
+
+</html>
